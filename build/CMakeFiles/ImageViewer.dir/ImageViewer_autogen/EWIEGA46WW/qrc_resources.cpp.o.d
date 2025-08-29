21fe653263421f646abf37cc5d869098fddd058e
YaznAlbalawi11 <126675362+yazn1q3@users.noreply.github.com>
2025-08-29 17:43:35 +0300

    Delete build directory

--- build/CMakeFiles/ImageViewer.dir/ImageViewer_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ImageViewer.dir/ImageViewer_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/yazn/sounds/build/ImageViewer_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/ImageViewer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ImageViewer.
-# This may be replaced when dependencies are built.
